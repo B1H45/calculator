c1d8d7ec2f49f7cf29b43ce66840bcd44826f6b7
Ben Hustler <ben1hustler@gmail.com>
2025-12-27 03:06:36 +0000

    All done except for decimal point

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ let operate = function() {
             result = val1 * val2;
             break;
         case "divide":
-            result = (val1 / val2).toFixed(4);
+            result = parseFloat((val1 / val2).toFixed(4));
             break;
     }
 
     console.log(result);
     val1 = result;
-    val2 = null;
+    val2 = 0;
     op = null;
     return result;
 }
@@ -38,15 +38,13 @@ let setNumber = function(num) {
     } else {
         val1 = num;
     }
-    
-    console.log(`${val1}, ${val2}`);
 }
 
 let setOp = function(setOp) {
-    if (setOp == "equals") {
+    if (setOp.id == "equals") {
         operate();
     }
-    if (val1 && val2 && op) {
+    else if (val1 && val2 && op) {
         operate();
         op = setOp;
     } else {
@@ -56,17 +54,25 @@ let setOp = function(setOp) {
 }
 
 let mainRow = document.getElementById("mainRow");
-let topRow = document.getElementById("topRow");
+// let topRow = document.getElementById("topRow");
+let thing = document.getElementById("thing");
 
 let updateDisplay = function() {
     mainRow.textContent = val1;
-    topRow.textContent = val2;
-    if (val2 == 0) {
-        topRow.textContent = "";
+    // topRow.textContent = val2;
+    if (val1 == 0 && val2 != 0 && val2 != null) {
+        // mainRow.textContent = topRow.textContent;
+        // topRow.textContent = "";
+    }
+    if (val2 == 0 || val1 == 0) {
+        // topRow.textContent = "";
+        
     }
     if (op && val2 && val1 && op.innerHTML != "=") {
-        topRow.textContent = `${val2} ${op.innerHTML}`;
+        // topRow.textContent = `${val2} ${op.innerHTML}`;
     }
+
+    thing.textContent = `${val1}, ${val2}, ${op.id}`;
 }
 
 let numButtons = document.querySelectorAll(".numButton");
@@ -79,6 +85,26 @@ numButtons.forEach((element) => {
 
 });
 
+let clearButton = document.querySelector("#clear");
+let delButton = document.querySelector("#delete");
+
+
+clearButton.addEventListener("click", () => {
+    val1 = null;
+    val2 = null;
+    op = null;
+    updateDisplay();
+});
+
+
+delButton.addEventListener("click", () => {
+    val1 = Math.floor(val1/10);
+    if (val1 == 0) {
+        val1 = null;
+    }
+    updateDisplay();
+});
+
 
 let opButtons = document.querySelectorAll(".opButton");
 opButtons.forEach((element) => {
@@ -89,3 +115,5 @@ opButtons.forEach((element) => {
     })
 
 });
+
+
