c837688ff5bc0c0807ee71eb29654eb3eff5ee66
Ben Hustler <ben1hustler@gmail.com>
2025-12-28 05:36:10 +0000

    Finished the hover effects

--- script.js
+++ script.js
@@ -3,29 +3,37 @@ console.log("HELLO");
 let val1;
 let val2;
 let op;
+let nope = false;
 
 let operate = function() {
     let result;
 
-    switch (op.id) {
+    switch (op.parentElement.id) {
         case "add":
-            result = parseFloat((val2 + val1).toFixed(10));
+            result = parseFloat((val2 + val1).toFixed(8));
             break;
         case "subtract":
-            result = parseFloat((val2 - val1).toFixed(10));
+            result = parseFloat((val2 - val1).toFixed(8));
             break;
         case "multiply":
-            result = parseFloat((val2 * val1).toFixed(10));
+            result = parseFloat((val2 * val1).toFixed(8));
             break;
         case "divide":
-            result = parseFloat((val2 / val1).toFixed(10));
+            result = parseFloat((val2 / val1).toFixed(8));
             break;
     }
 
     console.log(result);
+    if (!Number.isFinite(result)) {
+        val1 = 0;
+        nope = true;
+        updateDisplay();
+        return "NOPE!!!"
+    }
     val1 = result;
     val2 = 0;
     op = null;
+    
     return result;
 }
 
@@ -41,7 +49,7 @@ let setNumber = function(num) {
 }
 
 let setOp = function(setOp) {
-    if (setOp.id == "equals") {
+    if (setOp.parentElement.id == "equals") {
         operate();
     }
     else if (val1 && val2 && op) {
@@ -57,29 +65,37 @@ let mainRow = document.getElementById("mainRow");
 // let topRow = document.getElementById("topRow");
 let thing = document.getElementById("thing");
 
-let updateDisplay = function() {
-    mainRow.textContent = val1;
-    // topRow.textContent = val2;
-    if (val1 == 0 && val2 != 0 && val2 != null) {
-        // mainRow.textContent = topRow.textContent;
-        // topRow.textContent = "";
+let updateDisplay = function(str) {
+    if (nope) {
+        mainRow.textContent = "NOPE!!!";
     }
-    if (val2 == 0 || val1 == 0) {
-        // topRow.textContent = "";
-        
-    }
-    if (op && val2 && val1 && op.innerHTML != "=") {
-        // topRow.textContent = `${val2} ${op.innerHTML}`;
+    else {
+        mainRow.textContent = val1;
+        // topRow.textContent = val2;
+        if (val1 == 0 && val2 != 0 && val2 != null) {
+            // mainRow.textContent = topRow.textContent;
+            // topRow.textContent = "";
+        }
+        if (val2 == 0 || val1 == 0) {
+            // topRow.textContent = "";
+            
+        }
+        if (op && val2 && val1 && op.innerHTML != "=") {
+            // topRow.textContent = `${val2} ${op.innerHTML}`;
+        }
+
+        // thing.textContent = `Val1, val2, op: ${val1}, ${val2}, ${op.id}`;
     }
 
-    thing.textContent = `Val1, val2, op: ${val1}, ${val2}, ${op.id}`;
+
 }
 
 let numButtons = document.querySelectorAll(".numButton");
 numButtons.forEach((element) => {
 
     element.addEventListener("click", (e) => {
-        setNumber(Number(e.target.innerHTML));
+        setNumber(Number(element.children[0].textContent));
+        console.log(e.target.textContent);
         updateDisplay();
     })
 
@@ -93,6 +109,7 @@ clearButton.addEventListener("click", () => {
     val1 = null;
     val2 = null;
     op = null;
+    nope = false;
     updateDisplay();
 });
 
@@ -146,7 +163,7 @@ let buttons = document.querySelectorAll("button");
 buttons.forEach(
     (button) => {
         let buttonColoring = document.createElement("div");
-        button.prepend(buttonColoring);
+        button.append(buttonColoring);
         buttonColoring.classList.add("buttonColoring");
 
         let rect = button.getBoundingClientRect();
@@ -154,8 +171,8 @@ buttons.forEach(
         let posY;
 
         document.addEventListener("mousemove", (e) => {
-            posX = mouseX - rect.left;
-            posY = mouseY - rect.top;
+            posX = mouseX - rect.left - rect.width/2;
+            posY = mouseY - rect.top - rect.height/2;
 
             // const angle = Math.atan2(mouseY - (rect.top+rect.height/4), mouseX - (rect.left+rect.width/4));
             // button.children[0].style.transform = `rotate(${angle*(180/Math.PI) +45}deg)`;
@@ -163,14 +180,37 @@ buttons.forEach(
         });
 
         button.addEventListener("mouseenter", () => {
-            button.children[0].style.left = `calc(${posX}px - 5rem)`;
-            button.children[0].style.top = `calc(${posY}px - 5rem)`;
+            let xOffset = Math.cos(Math.atan2(posY, posX))*200;
+            let yOffset = Math.sin(Math.atan2(posY, posX))*200;
 
+            if (button.id == "clear") {
+                console.log(`xOffset: ${xOffset}, yOffset: ${yOffset}`);
+            }
+
+            button.children[1].classList.add("disableTransition");
             
+            button.children[1].style.left = `calc((${xOffset + rect.width/2}px - 5rem))`;
+            button.children[1].style.top = `calc((${yOffset + rect.height/2}px - 5rem))`;
+        
+            follower.style.transform = "translate(-50%, -50%) scale(0.1)";
+            follower.style.transition = "transform .5s ease-out";
+
             setTimeout(() => {
-                button.children[0].style.left = "calc(-5rem + 50%)";
-                button.children[0].style.top = "calc(-5rem + 50%)";
-            }, 500);
+                button.children[1].classList.remove("disableTransition");
+                button.children[1].style.left = "calc(50% - 5rem)";
+                button.children[1].style.top = "calc(50% - 5rem)";
+            }, 10);
+
+        });
+
+        button.addEventListener("mouseleave", () => {
+            let xOffset = Math.cos(Math.atan2(posY, posX))*200;
+            let yOffset = Math.sin(Math.atan2(posY, posX))*200;
+
+            button.children[1].style.left = `calc((${xOffset + rect.width/2}px - 5rem))`;
+            button.children[1].style.top = `calc((${yOffset + rect.height/2}px - 5rem))`;
+
+            follower.style.transform = "translate(-50%, -50%)";
         });
     }
 );
